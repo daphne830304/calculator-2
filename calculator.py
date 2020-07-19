532766bd7035f5321ad8371f1d29c98639650ae6
Daphne Hsieh <dhsieh@revmed.com>
2020-07-18 17:37:59 -0700

    Further study

--- calculator.py
+++ calculator.py
@@ -3,33 +3,48 @@
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
+from functools import reduce
 
 
 # Replace this with your code
-while True:
-    userinput = input('Enter numbers:')
-    token = userinput.split(' ')      
-    oper = token[0]
-    if len(token) == 2:
-        num1 = int(token[1])
-    if len(token) == 3:
-        num1 = int(token[1])
-        num2 = int(token[2])
-    if token[0] == 'q':
-        break
-    elif token[0] == "+":
-        add(num1, num2)
-    elif token[0] == "-":
-        subtract(num1, num2)
-    elif oper == "square":
-        print(square(num1))
-    elif oper == "*":
-        print(multiply(num1,num2))
-    elif oper == "/":
-        print(divide(num1,num2))
-    elif oper == "cube":
-        print(cube(num1))
-    elif oper == "pow":
-        print(power(num1, num2))
-    elif oper == "mod":
-        print(mod(num1,num2))
+def calculator():
+    while True:
+        userinput = input('Enter numbers:')
+        token = userinput.split(' ')      
+        oper = token[0]
+        if len(token) == 2:
+            num1 = int(token[1])
+        if len(token) == 3:
+            num1 = int(token[1])
+            num2 = int(token[2])
+        if len(token) >3:
+            print('incorrect input')
+        if token[0] == 'q':
+            break
+        elif token[0] == "+":
+            add(num1, num2)
+        elif token[0] == "-":
+            subtract(num1, num2)
+        elif oper == "square":
+            print(square(num1))
+        elif oper == "*":
+            print(multiply(num1,num2))
+        elif oper == "/":
+            print(divide(num1,num2))
+        elif oper == "cube":
+            print(cube(num1))
+        elif oper == "pow":
+            print(power(num1, num2))
+        elif oper == "mod":
+            print(mod(num1,num2))
+
+#def further():
+    while True:
+        userinput = input('Enter numbers:')
+        token = userinput.split(' ')      
+        oper = token[0]
+        num1 = float(token[1])
+        if len(token) == 3:
+            num2 = float(token[2])
+        if len(token) >= 3:
+            print('invalid entry')
